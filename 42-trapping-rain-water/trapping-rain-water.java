e2a9c7b374060aa81803a8ce2314dbeed873f18e
Km-reshma <RESHMAK0811@GMAIL.COM>
2026-03-11 13:12:10 +0530

    Time: 1 ms (59.99%) | Memory: 47.9 MB (56.12%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,68 +1,37 @@
 class Solution {
-   
-       public int trap(int[] height) {
-//         int left = 0;
-//         int right = height.length - 1;
-//         int leftMax = 0;
-//         int rightMax = 0;
-//         int water = 0;
 
-//         while (left < right) {
-//             if (height[left] < height[right]) {
-//                 if (height[left] >= leftMax) {
-//                     leftMax = height[left];
-//                 } else {
-//                     water += leftMax - height[left];
-//                 }
-//                 left++;
-//             } else {
-//                 if (height[right] >= rightMax) {
-//                     rightMax = height[right];
-//                 } else {
-//                     water += rightMax - height[right];
-//                 }
-//                 right--;
-//             }
-//         }
+    public int trap(int[] height) {
 
-//         return water;
-          
-        int n = height.length ; 
-        
-        int[] Left = new int[n] ;
-        int[] Right = new int[n] ;
+        int n = height.length;
 
-        Left[0] =  height[0] ;
-        for(int i = 1 ; i < n ; i++)
-        {
-            Left[i] = Math.max(Left[i-1],height[i]) ;
+        int i = 0;
+        int j = n - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int area = 0;
+
+        while (i < j) {
+
+            if (height[i] > leftMax) {
+                leftMax = height[i];
+            }
+
+            if (height[j] > rightMax) {
+                rightMax = height[j];
+            }
+
+            if (leftMax < rightMax) {
+                area += leftMax - height[i];
+                i++;
+            } 
+            else {
+                area += rightMax - height[j];
+                j--;
+            }
         }
 
-        Right[n-1] = height[n-1] ;
-        for(int i = n-2 ; i >= 0 ; i--)
-        {
-            Right[i] = Math.max(Right[i+1],height[i]) ;
-        }
-
-        int Area = 0 ;
-        for(int i = 0 ; i < n ; i++)
-        {
-            Area += Math.min(Right[i],Left[i])-height[i] ;
-        }
-        return Area ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return area;
     }
 }
